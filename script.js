32182c32579173c94cce5b0bf90b997aa7212f86
Yasshhh12 <prasyast152@gmail.com>
2024-06-05 01:01:20 +0530

    solved the bug for not fetching pronunciation

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ btn.addEventListener("click", () => {
                 </div>
                 <div class="details">
                     <p>${data[0].meanings[0].partOfSpeech}</p>
-                    <p>${data[0].phonetic}</p>
+                    <p>${data[0].phonetics[1].text}</p>
                 </div>
                 <p class="word-meaning">
-                   ${data[0].meanings[0].definitions[0].definition}
+                   ${data[0].meanings[2].definitions[0].definition}
                 </p>
                 <p class="word-example">
                     ${data[0].meanings[0].definitions[0].example || ""}
